0dd45b75cdd31079126950794dc8ee047cb010e0
xiaoiver <pyqiverson@gmail.com>
2018-03-14 17:20:42 +0800

    add runtime cache for imgs

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?v=1521018079').then(function(reg) {
+            navigator.serviceWorker.register('/service-worker.js?v=1521018811').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
