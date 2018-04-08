b04642b641a81d428e11e23b9ec34e791410f71e
潘宇琪 <panyuqi@panyuqideMacBook-Pro.local>
2018-04-09 00:10:14 +0800

    add post: font

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?v=1523115870').then(function(reg) {
+            navigator.serviceWorker.register('/service-worker.js?v=1523203754').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
