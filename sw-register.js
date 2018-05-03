9bf46247ee4b2c12fdbb2843bc3848738db4c38c
xiaoiver <pyqiverson@gmail.com>
2018-05-03 16:23:56 +0800

    add post

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?v=1524848521').then(function(reg) {
+            navigator.serviceWorker.register('/service-worker.js?v=1525335775').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
