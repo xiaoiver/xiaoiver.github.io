d9bde5971514b253bedcad50320a295f1e841365
潘宇琪 <panyuqi@panyuqideMacBook-Pro.local>
2018-05-29 08:54:32 +0800

    add post

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?v=1525335775').then(function(reg) {
+            navigator.serviceWorker.register('/service-worker.js?v=1527555177').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
