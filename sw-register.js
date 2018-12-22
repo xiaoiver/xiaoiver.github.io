95067d2842fd9f12a2c9c73db6c20e836bed70e5
xiaoiver <pyqiverson@gmail.com>
2018-12-22 23:14:02 +0800

    merge

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?v=1545406650').then(function(reg) {
+            navigator.serviceWorker.register('/service-worker.js?v=1545490837').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
