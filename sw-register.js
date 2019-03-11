01c3410adeffd8368e41e57399b5a670732ff1ff
yuqi.pyq <yuqi.pyq@antfin.com>
2019-03-11 09:52:21 +0800

    feat: update pic

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?v=1549698144').then(function(reg) {
+            navigator.serviceWorker.register('/service-worker.js?v=1552269066').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
